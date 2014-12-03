e4f68048c602042116b7114bccf34acb208de0ea
Adam Gardiner <adam.b.gardiner@gmail.com>
2014-12-03 16:32:11 +0000

    Add test for extracts

---
+++ test/test_extract.rb
@@ -0,0 +1,38 @@
+require 'test/unit'
+require 'ess4r'
+require_relative 'credentials'
+require 'fileutils'
+
+
+class TestExtract < Test::Unit::TestCase
+
+    OUTPUT_DIR = File.dirname(__FILE__) + '/../data/'
+
+    FileUtils.mkdir_p(OUTPUT_DIR)
+
+
+    EXTRACT_SPEC = {
+        rows: {
+            Accounts: 'Accounts.Level0',
+            Product: 'Product.Level0',
+            Market: 'East.Children',
+            Scenario: 'Actual'
+        },
+        columns: {
+            Year: 'Year.Level0'
+        }
+    }
+
+
+    def setup
+        srv = Essbase.connect(ESSBASE_USER, ESSBASE_PWD, ESSBASE_SERVER)
+        @cube = srv.open_cube('Demo', 'Basic')
+    end
+
+    def test_mdx_extract
+        @cube.extract(EXTRACT_SPEC, OUTPUT_DIR + 'mdx_extract.txt',
+                      include_headers: true, query_file: OUTPUT_DIR + 'mdx_extract.mdx')
+    end
+
+end
+
