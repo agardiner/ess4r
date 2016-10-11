36a7a78102b5b608789c316a881ec3a5ac37e2fa
Adam Gardiner <adam.b.gardiner@gmail.com>
2016-10-11 12:12:37 +0100

    Be more flexible with start_time for log retrieval

--- lib/ess4r/application.rb
+++ lib/ess4r/application.rb
@@ -23,12 +23,18 @@ class Essbase
         # @param local_file [String] The path to the local file to save the log to
         # @param start_time [Date, Time, DateTime] The earliest time from which to
         #    return log entries; if omitted, the entire log file is returned.
-        def get_log_file(local_file, start_time = 0)
+        def get_log_file(local_file, start_time = nil)
             case start_time
             when Date, DateTime
                 start_time = start_time.to_time.to_i
             when Time
                 start_time = start_time.to_i
+            when NilClass
+                start_time = 0
+            when Fixnum
+                # Pass as is
+            else
+                raise ArgumentError, "Expected a Date, Time, or DateTime object; got #{start_time.class}"
             end
             try{ m_olapCxt.orbPlugin.essMainGetLogFile(m_olapCxt.hCtx, getName(), start_time, local_file) }
         end
